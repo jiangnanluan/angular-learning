39ff560588beb8b328580b9871826e90856f4ea5
jiangnanluan <jiangnanluan@users.noreply.github.com>
2018-03-02 15:11:33 +0800

    Update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,14 @@ import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {FormsModule} from '@angular/forms';
 
+import {AppRoutingModule} from "./app-routing.module";
+
 import {AppComponent} from './app.component';
 
 import {HeroService} from './hero/hero.service';
 import {HeroesComponent} from "./hero/heroes.component";
-import {HeroDetailComponent} from './hero/hero-detail.component';
 import {DashboardComponent} from "./hero/dashboard.component";
-import {AppRoutingModule} from "./app-routing.module";
+import {HeroDetailComponent} from './hero/hero-detail.component';
 
 @NgModule({
   declarations: [
